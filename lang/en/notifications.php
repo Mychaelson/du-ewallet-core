c3fa1b74cfcf8a34c40809130faa2ebd604a07de
Mychael Son <98306105+Mychaelson@users.noreply.github.com>
2022-12-01 13:52:58 +0700

    change otp app name

--- lang/en/notifications.php
+++ lang/en/notifications.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'otp-notifications' => ':token is the OTP for Yamisok apps. OTP is valid for 5 mins. Please do not share it with anyone.',
+    'otp-notifications' => ':token is the OTP for Dupay apps. OTP is valid for 5 mins. Please do not share it with anyone.',
 ];
